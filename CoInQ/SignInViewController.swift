b10929ab174cc1a12f086a89bb3892e75d26dc66
hui <hui@cchuimacbookair.local>
2017-05-11 21:21:57 +0800

    just formate changing test

--- CoInQ/SignInViewController.swift
+++ CoInQ/SignInViewController.swift
@@ -11,8 +11,6 @@ import UIKit
 class SignInViewController: UIViewController,GIDSignInUIDelegate,GIDSignInDelegate
 {
     @IBOutlet weak var welcomeLable: UILabel!
-
-
     
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
         
@@ -30,7 +28,6 @@ class SignInViewController: UIViewController,GIDSignInUIDelegate,GIDSignInDelega
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         GIDSignIn.sharedInstance().uiDelegate = self
         GIDSignIn.sharedInstance().delegate = self
         GIDSignIn.sharedInstance().signInSilently()
