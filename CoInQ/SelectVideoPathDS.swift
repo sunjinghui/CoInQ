5db82104d6e68a40dd36b2f6040cd135df2deb01
hui <hui@cchuimacbookair.local>
2017-05-15 18:02:45 +0800

    ADD select video code and data structure of the video url path

---
+++ CoInQ/SelectVideoPathDS.swift
@@ -0,0 +1,15 @@
+//
+//  SelectVideoPath_DS.swift
+//  CoInQ
+//
+//  Created by hui on 2017/5/15.
+//  Copyright © 2017年 NTNUCSCL. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct SelectVideoPathDS {
+    var firstAsset: AVAsset!
+    var secondAsset: AVAsset!
+}
