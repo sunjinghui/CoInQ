a0443df84da5631a11c5219e728acff17cf7f84f
hui <hui@cchuimacbookair.local>
2017-06-18 19:49:26 +0800

    delete audioRecorderVC

--- CoInQ/RecordAudio_OneTwo.swift
+++ CoInQ/RecordAudio_OneTwo.swift
@@ -11,11 +11,6 @@ import AVFoundation
 
 class RecordAudio_OneTwo: UIViewController , AudioRecorderViewControllerDelegate {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
