4dc8734e3800f2612a58abbfbdad1a53f21d68b7
hui <hui@cchuimacbookair.local>
2017-09-18 17:03:21 +0800

    Change the version of the Helper_VC detail:  show the texts in the middle of the 5E circle, and keep the button highlighted when it's selected

--- CoInQ/Helper_VC.swift
+++ CoInQ/Helper_VC.swift
@@ -10,50 +10,92 @@ import Foundation
 
 class Helper_VC: UIViewController {
     
+    @IBOutlet weak var HelpfultextView: UIView!
+    
+    @IBOutlet weak var Helpfultext: UILabel!
+    @IBOutlet weak var stageone: UIButton!
+    @IBOutlet weak var stagetwo: UIButton!
+    @IBOutlet weak var stagethree: UIButton!
+    @IBOutlet weak var stagefour: UIButton!
+    @IBOutlet weak var stagefive: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        HelpfultextView.isHidden = true
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    func orignal() {
+        if stageone.isSelected == false {
+            stageone.setImage(#imageLiteral(resourceName: "circle1"), for: UIControlState())
+        }
+        if stagetwo.isSelected == false {
+            stagetwo.setImage(#imageLiteral(resourceName: "circle2"), for: UIControlState())
+        }
+        if stagethree.isSelected == false {
+            stagethree.setImage(#imageLiteral(resourceName: "circle3"), for: UIControlState())
+        }
+        if stagefour.isSelected == false {
+            stagefour.setImage(#imageLiteral(resourceName: "circle4"), for: UIControlState())
+        }
+        if stagefive.isSelected == false {
+            stagefive.setImage(#imageLiteral(resourceName: "circle5"), for: UIControlState())
+        }
+    }
+    
+    @IBAction func StageOne(_ sender: Any) {
+        orignal()
+        if HelpfultextView.isHidden == true {
+            HelpfultextView.isHidden = false
+        }
+        Helpfultext.text = "科學探究皆從\n一個問題開始。"
+        stageone.setImage(#imageLiteral(resourceName: "circle1_1"), for: UIControlState())
+    }
+    
+    @IBAction func StageTwo(_ sender: Any) {
+        orignal()
+        if HelpfultextView.isHidden == true {
+         HelpfultextView.isHidden = false
+        }
+        Helpfultext.text = "能夠確實回答問題\n的「資料」\n稱為「證據」。"
+        stagetwo.setImage(#imageLiteral(resourceName: "circle2_1"), for: UIControlState())
+    }
+    
+    @IBAction func StageThree(_ sender: Any) {
+        orignal()
+        if HelpfultextView.isHidden == true {
+            HelpfultextView.isHidden = false
+        }
+        Helpfultext.text = "分析、比較「證據」\n便能得出可能的解釋。"
+        stagethree.setImage(#imageLiteral(resourceName: "circle3_1"), for: UIControlState())
+    }
+    
+    @IBAction func StageFour(_ sender: Any) {
+        orignal()
+        if HelpfultextView.isHidden == true {
+            HelpfultextView.isHidden = false
+        }
+        Helpfultext.text = "最終解釋應\n符合科學、邏輯\n的標準。"
+        stagefour.setImage(#imageLiteral(resourceName: "circle4_1"), for: UIControlState())
+    }
+    
+    @IBAction func StageFive(_ sender: Any) {
+        orignal()
+        if HelpfultextView.isHidden == true {
+            HelpfultextView.isHidden = false
+        }
+        Helpfultext.text = "積極分享是勇氣，\n願意回饋是美德。"
+        stagefive.setImage(#imageLiteral(resourceName: "circle5_1"), for: UIControlState())
+    }
+    
+/*    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "segue_one" {
             segue.destination.popoverPresentationController?.sourceRect = (sender as! UIView).bounds
             segue.destination.popoverPresentationController?.delegate = self
         }
-        if segue.identifier == "segue_two" {
-            segue.destination.popoverPresentationController?.sourceRect = (sender as! UIView).bounds
-            segue.destination.popoverPresentationController?.delegate = self
-        }
-        if segue.identifier == "segue_three" {
-            segue.destination.popoverPresentationController?.sourceRect = (sender as! UIView).bounds
-            segue.destination.popoverPresentationController?.delegate = self
-        }
-        if segue.identifier == "segue_four" {
-            segue.destination.popoverPresentationController?.sourceRect = (sender as! UIView).bounds
-            segue.destination.popoverPresentationController?.delegate = self
-        }
-        if segue.identifier == "segue_five" {
-            segue.destination.popoverPresentationController?.sourceRect = (sender as! UIView).bounds
-            segue.destination.popoverPresentationController?.delegate = self
-        }
-    }
+    }*/
     
-}
-
-extension Helper_VC: UIPopoverPresentationControllerDelegate {
-    
-    func popoverPresentationControllerDidDismissPopover(_ popoverPresentationController: UIPopoverPresentationController){
-        print("popover dismissed")
-    }
-    
-    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
-        return .none
-    }
-    
-    func popoverPresentationControllerShouldDismissPopover(_ popoverPresentationController: UIPopoverPresentationController) -> Bool {
-            return true
-    }
 }
